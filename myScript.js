86954d17687769cf58b3f72d9918690f21108d96
MortaXN <106821355+MortaXN@users.noreply.github.com>
2022-06-06 11:36:32 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,6 +2,7 @@ function ShowHidden(){
   document.getElementById("Load").innerHTML = "MortaXN is just my Pseudonym. My Real Name is... Secret :b";
 }
 
+onload = Alert();
 function Alert(){
   alert('Most Info is Fictional, Proceed At Your Own Risk');
 }
