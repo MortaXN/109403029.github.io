3d52dc1ed858e69e89418be0575605503536782e
MortaXN <106821355+MortaXN@users.noreply.github.com>
2022-06-06 11:32:45 +0800

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function ShowHidden(){
+  document.getElementById("Load").innerHTML = "MortaXN is just my Pseudonym. My Real Name is... Secret :b";
+}
+
+function Alert(){
+  alert('Most Info is Fictional, Proceed At Your Own Risk');
+}
